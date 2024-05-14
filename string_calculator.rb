3bd8924e7892acb134a62f759ca3b3c545e789cb
Mahesh Lamdade <mahesh.12lamdade@gmail.com>
2024-05-14 11:35:44 +0530

    New line characters normalised, invalid input case handled

--- string_calculator.rb
+++ string_calculator.rb
@@ -10,6 +10,12 @@ class StringCalculator
       input = input[(input.index("\n") + 1)..-1]
     end
 
+    # Remove new line characters & replace them with the current delimiter
+    input.gsub!("\n", delimiter)
+
+    # Check if the last character is delimiter, if yes raise error
+    raise "Invalid input" if input.end_with?(delimiter)
+
     # Split the string using delimiter and convert the relevant string(s) into integer
     numbers = input.split(delimiter).map(&:to_i)
     numbers.sum
