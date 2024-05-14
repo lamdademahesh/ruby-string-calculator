3d79b6a92916ea16701ac7d45dad8c4da161fae0
Mahesh Lamdade <mahesh.12lamdade@gmail.com>
2024-05-14 11:27:03 +0530

    Delimiter updated if custom delimiter passed in param

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,14 @@ class StringCalculator
     # set default delimiter
     delimiter = ","
 
+    # Check if there exists any custom delimiter in the passed param
+    # and update the delimiter if required
+    if input.start_with?("//")
+      delimiter = input[2]
+      input = input[(input.index("\n") + 1)..-1]
+    end
+
+    # Split the string using delimiter and convert the relevant string(s) into integer
     numbers = input.split(delimiter).map(&:to_i)
     numbers.sum
   end
